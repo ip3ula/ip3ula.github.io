f5b09396f3068c5a579dbf23cc187304df49f9be
Paula Emad <ip3ula@hotmail.com>
2025-03-26 21:34:30 +0200

    deploy website

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/ip3ula.github.io/',  // Ensure correct paths on GitHub Pages
-  server: {
-    host: true,
-    port: 5173,
-  },
-  build: {
-    outDir: 'dist', 
-  }
-});
+  plugins: [react()
+    , tailwindcss()
+  ],
+})
