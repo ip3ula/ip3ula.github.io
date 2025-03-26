4d08eef26b65cca7403ee973e75696f9e928b71a
Paula Emad <ip3ula@hotmail.com>
2025-03-26 21:24:13 +0200

    fixes bug

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()
-    , tailwindcss()
-  ],
-})
+  plugins: [react(), tailwindcss()],
+  base: '/ip3ula.github.io/',  // Ensure correct paths on GitHub Pages
+  server: {
+    host: true,
+    port: 5173,
+  },
+  build: {
+    outDir: 'dist', 
+  }
+});
